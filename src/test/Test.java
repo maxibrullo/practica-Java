15539628fccc5be111831ca3a71aa35adb63b878
Maximiliano Brullo <maxibrullo@outlook.com>
2022-09-02 20:46:14 -0300

    change in test

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,11 +9,12 @@ public class Test {
         Empresa empresa1 = new Concesionario("Chrisley", 10978153, 14,
                 "05/06/1885", "Bv San Juan", 6);
         imprimir(empresa1);
+        System.out.println();
         Empresa empresa2 = new Fabrica("Chrisley", 5, "02/09/1996",
                 "Mariano Moreno", 10659);
         imprimir(empresa2);
     }
     public static void imprimir(Empresa empresa){
-        empresa.toString();
+        System.out.print(empresa.toString());
     }
 }
